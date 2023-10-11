2afa8ea60ac4810841d5394abb3e150e92e4ff34
Linicker <olinicker@gmail.com>
2023-10-11 10:54:50 -0300

    c2 - criando função empréstimo

--- AtividadeCatalogo/Program.cs
+++ AtividadeCatalogo/Program.cs
@@ -9,15 +9,10 @@ class Program
         public string console;
         public int ano;
         public int ranking;
-    }
-
-    struct emprestimo
-    {
         public int dataEmprestimo;
         public string nomePessoaEmprestimo;
         public char emprestado;
     }
-
     static void Cadastrar(List<jogo> lista)
     {
         jogo novoJogo = new jogo();
@@ -30,10 +25,50 @@ class Program
         novoJogo.ano = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Qual o ranking:");
         novoJogo.ranking = Convert.ToInt32(Console.ReadLine());
+
+        lista.Add(novoJogo);
+        Console.WriteLine("Jogo cadastrado com sucesso!");
+    }
+
+    static void Emprestimo(List<jogo> lista)
+    {
+        Console.WriteLine("Qual jogo deseja realizar empréstimo?");
+        string nomeBusca = Console.ReadLine();
+        char confirma;
+        int qtd = lista.Count();
+        jogo emprestimo = new jogo();
+
+        for (int i = 0;i<qtd; i++)
+        {
+            if (lista[i].titulo.ToUpper().Contains(nomeBusca.ToUpper()))
+            {
+                Console.WriteLine($"Deseja mesmo realizar emprestimo de {nomeBusca}? S/N");
+                confirma = Convert.ToChar(Console.ReadLine());
+
+                if(confirma == 's' || confirma == 'S')
+                {
+                    Console.WriteLine("Qual a data do emprestimo:");
+                    emprestimo.dataEmprestimo = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Qual o nome da pessoa:");
+                    emprestimo.nomePessoaEmprestimo = Console.ReadLine();
+                    emprestimo.emprestado = 'S';
+                }
+                else
+                {
+                    Console.WriteLine("Operação cancelada");
+                    emprestimo.emprestado = 'N';
+                }
+                
+
+            }
+        }
     }
     static void Main()
     {
         List<jogo> lista = new List<jogo>();
+
+
+
     }
 
 }

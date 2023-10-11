dcb4d4345e78d81ea6b4535395b52ebabcc968de
Linicker <olinicker@gmail.com>
2023-10-11 11:25:15 -0300

    c3 - melhorando funções, separando em dois structs, criando funções listarEmprestimo e Menu.

--- AtividadeCatalogo/Program.cs
+++ AtividadeCatalogo/Program.cs
@@ -9,9 +9,38 @@ class Program
         public string console;
         public int ano;
         public int ranking;
+    }
+
+    struct emprestimo
+    {
+        public string tituloJogo;
         public int dataEmprestimo;
         public string nomePessoaEmprestimo;
         public char emprestado;
+    }
+
+    static void listarJogos(List<jogo> lista)
+    {
+
+        foreach(jogo j in lista)
+        {
+            Console.WriteLine("Nome do jogo:" + j.titulo);
+            Console.WriteLine("Console:" + j.console);
+            Console.WriteLine("Ano de lançamento:" + j.ano);
+            Console.WriteLine("Ranking:" + j.ranking);
+        }
+
+    }
+
+    static void listarEmprestimo(List<emprestimo> lista)
+    {
+        foreach (emprestimo m in lista)
+        {
+            Console.WriteLine("Nome do jogo:" + m.tituloJogo);
+            Console.WriteLine("Data do empréstimo:" + m.dataEmprestimo);
+            Console.WriteLine("Nome da pessoa:" + m.nomePessoaEmprestimo);
+            Console.WriteLine("Emprestado? " + m.emprestado);
+        }
     }
     static void Cadastrar(List<jogo> lista)
     {
@@ -30,13 +59,13 @@ class Program
         Console.WriteLine("Jogo cadastrado com sucesso!");
     }
 
-    static void Emprestimo(List<jogo> lista)
+    static void Emprestimo(List<jogo> lista, List<emprestimo> listaEmp)
     {
         Console.WriteLine("Qual jogo deseja realizar empréstimo?");
         string nomeBusca = Console.ReadLine();
         char confirma;
         int qtd = lista.Count();
-        jogo emprestimo = new jogo();
+        emprestimo emp = new emprestimo();
 
         for (int i = 0;i<qtd; i++)
         {
@@ -48,26 +77,75 @@ class Program
                 if(confirma == 's' || confirma == 'S')
                 {
                     Console.WriteLine("Qual a data do emprestimo:");
-                    emprestimo.dataEmprestimo = Convert.ToInt32(Console.ReadLine());
+                    emp.dataEmprestimo = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Qual o nome da pessoa:");
-                    emprestimo.nomePessoaEmprestimo = Console.ReadLine();
-                    emprestimo.emprestado = 'S';
+                    emp.nomePessoaEmprestimo = Console.ReadLine();
+                    emp.emprestado = 'S';
+                    Console.WriteLine($"Emprestimo de {nomeBusca} realizado com sucesso!");
+                    emp.tituloJogo = nomeBusca;
+
+                    listaEmp.Add(emp);
                 }
+                
+                else if(confirma == 'n' || confirma == 'N')
+                {
+                    Console.WriteLine("Operação cancelada.");
+                }
+
                 else
                 {
-                    Console.WriteLine("Operação cancelada");
-                    emprestimo.emprestado = 'N';
+                    Console.WriteLine("Operação cancelada.");
                 }
                 
 
             }
         }
     }
+
+    static int Menu()
+    {
+        int op;
+
+        Console.WriteLine("1 - Cadastrar");
+        Console.WriteLine("2 - Listar");
+        Console.WriteLine("3 - Realizar emprestimo");
+        Console.WriteLine("4 - Listar Emprestimos");
+        Console.WriteLine("5 - Procurar por título");
+        Console.WriteLine("6 - Listar todos os jogos de um console");
+
+        op = Convert.ToInt32(Console.ReadLine());
+        return op;
+    }
     static void Main()
     {
         List<jogo> lista = new List<jogo>();
+        List<emprestimo> listaEmprestimo = new List<emprestimo>();
 
+        bool programa = true;
+        int operador;
+        do
+        {
+            operador = Menu();
+            switch (operador)
+            {
+                case 1:
+                    Cadastrar(lista);
+                    break;
+                case 2:
+                    listarJogos(lista);
+                    break;
+                case 3:
+                    Emprestimo(lista, listaEmprestimo);
+                    break;
+                case 4:
+                    listarEmprestimo(listaEmprestimo);
+                    break;
+            }
 
+            Console.ReadKey();
+            Console.Clear();
+
+        } while (programa);
 
     }
 
